4aae50b8a797a386fa332ec8736a150885a7fb1a
silvarge <sep.non2@gmail.com>
2025-06-24 10:22:42 +0900

    Feat: Feed 엔티티 및 Repository 추가 #15

---
+++ src/main/java/com/codeit/weatherwear/feed/entity/Feed.java
@@ -0,0 +1,67 @@
+package com.codeit.weatherwear.feed.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "feed")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feed {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private Instant createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+
+  @Column(name = "like_count")
+  private int likeCount = 0;
+
+  @Column(name = "comment_count")
+  private int commentCount = 0;
+
+  @Builder
+  private Feed(int likeCount, int commentCount) {
+    this.likeCount = likeCount;
+    this.commentCount = commentCount;
+  }
+
+  public void updateLikeCount(int likeCount) {
+    this.likeCount = likeCount;
+  }
+
+  public void increaseLikeCount() {
+    this.likeCount++;
+  }
+
+  public void decreaseLikeCount() {
+    this.likeCount--;
+  }
+
+  public void updateCommentCount(int commentCount) {
+    this.commentCount = commentCount;
+  }
+
+}
