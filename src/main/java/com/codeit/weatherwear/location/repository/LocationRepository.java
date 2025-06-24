45dddd348676f3a32198bc35a6da65fc85e9fee6
Soyoung Lee <soo7132@naver.com>
2025-06-24 09:32:16 +0900

    Feat: User 엔티티 추가 #12

--- src/main/java/com/codeit/weatherwear/location/repository/LocationRepository.java
+++ src/main/java/com/codeit/weatherwear/location/repository/LocationRepository.java
@@ -3,7 +3,9 @@ package com.codeit.weatherwear.location.repository;
 import com.codeit.weatherwear.location.entity.Location;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends JpaRepository<Location, UUID> {
 
 }
