48105f504877c956df7e9b1103fec4fedbedf68d
Soyoung Lee <soo7132@naver.com>
2025-06-24 15:05:45 +0900

    Feat: 회원가입 구현 TDD Green #21

--- src/main/java/com/codeit/weatherwear/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/codeit/weatherwear/domain/user/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.codeit.weatherwear.domain.user.service;
 
 import com.codeit.weatherwear.domain.user.dto.UserCreateRequest;
 import com.codeit.weatherwear.domain.user.dto.UserDto;
+import com.codeit.weatherwear.domain.user.entity.User;
+import com.codeit.weatherwear.domain.user.exception.UserAlreadyExistsException;
 import com.codeit.weatherwear.domain.user.mapper.UserMapper;
 import com.codeit.weatherwear.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,22 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto create(UserCreateRequest userCreateRequest) {
-        return null;
+        // 중복 검사
+        if (userRepository.existsByName(userCreateRequest.name()) ||
+            userRepository.existsByEmail(userCreateRequest.email())) {
+            throw new UserAlreadyExistsException();
+        }
+
+        User user = userRepository.save(
+            User.builder()
+                .name(userCreateRequest.name())
+                .email(userCreateRequest.email())
+                // TODO: Spring Security 적용 후 PasswordEncoder 적용
+                .password(userCreateRequest.password())
+                .locked(false)
+                .build()
+        );
+
+        return userMapper.toDto(user);
     }
 }
