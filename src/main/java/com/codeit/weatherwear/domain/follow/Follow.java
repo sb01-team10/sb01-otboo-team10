9a62ca891f65f11339e6f62a58826ff36d7d25ec
Oince <hky0105@naver.com>
2025-06-24 11:21:57 +0900

    Feat: 엔티티 추가

---
+++ src/main/java/com/codeit/weatherwear/domain/follow/Follow.java
@@ -0,0 +1,45 @@
+package com.codeit.weatherwear.domain.follow;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "follows", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"followee_id, follower_id"})
+})
+@Getter
+@NoArgsConstructor
+public class Follow {
+
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  // User 엔티티가 없어서 일단 주석처리 했습니다.
+//  @ManyToOne(fetch = FetchType.LAZY,  optional = false)
+//  @JoinColumn(name = "followee_id", nullable = false)
+//  private User followee;
+//
+//  @ManyToOne(fetch = FetchType.LAZY,  optional = false)
+//  @JoinColumn(name = "follower_id", nullable = false)
+//  private User follower;
+//
+//  private Follow(User followee, User follower) {
+//    this.followee = followee;
+//    this.follower = follower;
+//  }
+//
+//  public static Follow create(User followee, User follower) {
+//    return new Follow(followee, follower);
+//  }
+
+}
