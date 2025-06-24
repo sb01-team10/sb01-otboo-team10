52138ba9e0c04f19b6afa371c7b7b837c6402803
soyul <soyul9280@gmail.com>
2025-06-24 16:29:42 +0900

    [Refactor]속성등록 코드래빗 적용 - 트랜잭션추가 - 중복처리 개선

--- src/main/java/com/codeit/weatherwear/domain/clothes/service/AttributesServiceImpl.java
+++ src/main/java/com/codeit/weatherwear/domain/clothes/service/AttributesServiceImpl.java
@@ -5,8 +5,11 @@ import com.codeit.weatherwear.domain.clothes.dto.ClothesAttributeDefDto;
 import com.codeit.weatherwear.domain.clothes.entity.Attributes;
 import com.codeit.weatherwear.domain.clothes.mapper.AttributesMapper;
 import com.codeit.weatherwear.domain.clothes.repository.AttributesRepository;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +24,7 @@ public class AttributesServiceImpl implements AttributesService{
      * @return 속성DTO
      */
     @Override
+    @Transactional
     public ClothesAttributeDefDto create(ClothesAttributeDefCreateRequest request) {
         if(attributesRepository.existsByName(request.definitionName())){
             throw new IllegalArgumentException("이미 등록된 속성입니다.");
@@ -28,8 +32,9 @@ public class AttributesServiceImpl implements AttributesService{
         if(request.selectValues().isEmpty()){
             throw new IllegalArgumentException("값 입력은 필수입니다.");
         }
-        if (request.selectValues().size() != request.selectValues().stream().distinct().count()) {
-            throw new IllegalArgumentException("중복된 값을 입력하셨습니다.");
+        Set<String> uniqueValues = new HashSet<>(request.selectValues());
+        if (uniqueValues.size() != request.selectValues().size()) {
+            throw new IllegalArgumentException("중복된 값이 입력되었습니다.");
         }
 
         Attributes attributes = Attributes.builder()
