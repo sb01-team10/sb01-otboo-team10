2ca9e04954f21173ea1d8628f82aaa26cf440f2a
soyul <soyul9280@gmail.com>
2025-06-26 11:46:07 +0900

    [Refactor]예외 코드리뷰 적용

--- src/main/java/com/codeit/weatherwear/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codeit/weatherwear/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.codeit.weatherwear.global.exception;
 
 import com.codeit.weatherwear.global.response.ErrorResponse;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -24,10 +28,11 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(value = {NoHandlerFoundException.class,
       HttpRequestMethodNotSupportedException.class})
   public ResponseEntity<?> handleNoPageFoundException(Exception e) {
-    log.error("GlobalExceptionHandler catch NoHandlerFoundException : {}", e.getMessage());
+    //TODO: 배포 후 e.getMessage()로 바꾸기
+    log.error("GlobalExceptionHandler catch NoHandlerFoundException : {}", e);
     return ResponseEntity
         .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-        .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e,e.getClass().getSimpleName()));
+        .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e,Map.of("reason", "No handler or unsupported method")));
   }
 
   /**
@@ -36,13 +41,15 @@ public class GlobalExceptionHandler {
    * @param e
    * @return 해당 Error 코드에 대응하는 에러 응답
    */
-  @ExceptionHandler(value = {CustomException.class})
+  @ExceptionHandler
   public ResponseEntity<?> handleCustomException(CustomException e) {
     log.error("handleCustomException() in GlobalExceptionHandler throw CustomException : {}",
         e.getMessage());
+    ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode(),e,
+        e.getDetails());
     return ResponseEntity
         .status(e.getErrorCode().getStatus())
-        .body(ErrorResponse.of(e.getErrorCode(), e,e.getClass().getSimpleName()));
+        .body(errorResponse);
   }
 
   /**
@@ -51,13 +58,34 @@ public class GlobalExceptionHandler {
    * @param e
    * @return 500 INTERNAL SERVER ERROR 응답
    */
-  @ExceptionHandler(value = {Exception.class})
+  @ExceptionHandler
   public ResponseEntity<?> handleException(Exception e) {
+    //TODO: 배포 후 e.getMessage()로 바꾸기
     log.error("handleCustomException() in GlobalExceptionHandler throw Exception : {}",
-        e.getMessage());
+        e);
     return ResponseEntity
         .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-        .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e,e.getClass().getSimpleName()));
+        .body(ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e,Map.of("reason", "Unexpected error")));
   }
+
+  // Validation 예외
+  @ExceptionHandler
+  public ResponseEntity<?> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    BindingResult bindingResult = e.getBindingResult();
+
+    List<String> errors = bindingResult.getFieldErrors().stream()
+        .map(error -> String.format("[field=%s, rejected=%s, message=%s]",
+            error.getField(),
+            error.getRejectedValue(),
+            error.getDefaultMessage()))
+        .toList();
+
+    log.warn("Validation Failed: {}", errors);
+    return ResponseEntity
+        .status(ErrorCode.INVALID_INPUT_VALUE.getStatus())
+        .body(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE,e, Map.of("validationError", errors)));
+  }
+
 
 }
