2b4ae517ac14b1dea4129d353f034bb5f3d68da3
Soyoung Lee <soo7132@naver.com>
2025-06-26 18:11:32 +0900

    Test: 테스트 코드에 Spring Security 적용 추가

--- src/main/java/com/codeit/weatherwear/domain/security/config/SecurityConfig.java
+++ src/main/java/com/codeit/weatherwear/domain/security/config/SecurityConfig.java
@@ -2,31 +2,28 @@ package com.codeit.weatherwear.domain.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain chain(HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(HttpMethod.POST, "/api/users").permitAll() //회원가입
-                // TODO: 일단은 모든 요청 허용
-                .anyRequest().permitAll()
-            )
-            // TODO: csrf 활성화
-            .csrf(csrf -> csrf.disable())
-        ;
-        return httpSecurity.build();
-    }
+//    @Bean
+//    SecurityFilterChain chain(HttpSecurity httpSecurity) throws Exception {
+//
+//        httpSecurity
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers(HttpMethod.POST, "/api/users").permitAll() //회원가입
+//                // TODO: 일단은 모든 요청 허용
+//                .anyRequest().permitAll()
+//            )
+//            // TODO: csrf 활성화
+//            .csrf(csrf -> csrf.disable())
+//        ;
+//        return httpSecurity.build();
+//    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
